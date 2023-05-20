620745e1f925f5e2da7570d122df00816df71a20
John Cherry <github@johncherry.me>
2023-05-20 17:52:43 +0100

    Make it actually work

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,44 +1,42 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer").themes.github;
+const darkCodeTheme = require("prism-react-renderer").themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'York TechSoc Wiki',
-  tagline: 'Live Event Design, Install and Operation',
-  url: 'https://wiki.yorktechsoc.org',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  title: "York TechSoc Wiki",
+  tagline: "Live Event Design, Install and Operation",
+  url: "https://wiki.yorktechsoc.org",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'yorktechsoc', // Usually your GitHub org/user name.
-  projectName: 'wiki', // Usually your repo name.
-
-
+  organizationName: "yorktechsoc", // Usually your GitHub org/user name.
+  projectName: "wiki", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          path: 'src/docs'
+          sidebarPath: require.resolve("./sidebars.js"),
+          path: "src/docs",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -48,73 +46,73 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       announcementBar: {
-        id: 'wip',
+        id: "wip",
         content:
-          'We\'re having a bit of a makeover! Please excuse us whilst we update our website.',
-        backgroundColor: '#fafbfc',
-        textColor: '#091E42',
-        isCloseable: false,
+          "This is our internal wiki site, you may be looking for <a href='https://yorktechsoc.org'>our main site</a>",
+        backgroundColor: "#fafbfc",
+        textColor: "#091E42",
+        isCloseable: true,
       },
       navbar: {
-        title: 'York TechSoc Wiki',
+        title: "York TechSoc Wiki",
         logo: {
-          alt: 'York TechSoc Logo',
-          src: 'img/logo.svg',
+          alt: "York TechSoc Logo",
+          src: "img/logo.svg",
         },
         items: [
           {
-            label: 'Guides',
-            type: 'doc', 
-            docId: '/category/guides',
-            position:'left'
+            label: "Guides",
+            type: "doc",
+            docId: "/category/guides",
+            position: "left",
           },
           {
-            label: 'RAMS',
-            type: 'doc',
-            docId: '/category/RAMS',
-            position: 'left'
+            label: "RAMS",
+            type: "doc",
+            docId: "/category/rams",
+            position: "left",
           },
           {
-            label: 'AdamRMS',
-            href: 'www.dash.adam-rms.com',
-            position: 'right'
+            label: "AdamRMS",
+            href: "https://www.dash.adam-rms.com",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Social Media',
+            title: "Social Media",
             items: [
               {
-                label: 'Facebook',
-                href: 'https://www.facebook.com/yorktechsoc/',
+                label: "Facebook",
+                href: "https://www.facebook.com/yorktechsoc/",
               },
               {
-                label: 'Instagram',
-                href: 'https://www.instagram.com/uoytechsoc/',
+                label: "Instagram",
+                href: "https://www.instagram.com/uoytechsoc/",
               },
               {
-                label: 'Slack',
-                href: 'https://join.slack.com/t/yorktechsoc/shared_invite/zt-1df8p8flv-fqj2an8oJEx0n50Bjz~g~Q',
-              }
+                label: "Slack",
+                href: "https://join.slack.com/t/yorktechsoc/shared_invite/zt-1df8p8flv-fqj2an8oJEx0n50Bjz~g~Q",
+              },
             ],
           },
           {
-            title: 'With Thanks to',
+            title: "With Thanks to",
             items: [
               {
-                label: 'YUSU',
-                href: 'https://yusu.org/',
+                label: "YUSU",
+                href: "https://yusu.org/",
               },
               {
-                label: 'YUFUND',
-                href: 'https://www.york.ac.uk/york-unlimited/',
+                label: "YUFUND",
+                href: "https://www.york.ac.uk/york-unlimited/",
               },
               {
-                label: 'UoY AV',
-                to: 'https://www.york.ac.uk/about/departments/support-and-admin/estates-and-campus-services/audio-visual/',
+                label: "UoY AV",
+                to: "https://www.york.ac.uk/about/departments/support-and-admin/estates-and-campus-services/audio-visual/",
               },
             ],
           },
@@ -128,4 +126,4 @@ const config = {
     }),
 };
 
-module.exports = config ;
+module.exports = config;
---
+++ src/docs/guides/dixon.md
@@ -0,0 +1,27 @@
+---
+sidebar_position: 1
+title: Dixon Drama Studio
+---
+
+## Technical Specification
+
+### Lighting
+
+- 94no. channel 15a dimming
+- [Grid channel layout](/files/Dixon-grid-layout.pdf)
+- Selectable & dimmable permanent house lighting units
+- 4no. 5-pin DMX distribution at low-level facility panels
+- 1no. ETC Nomad kit (pending availability within the society)
+- Access to TechSoc's lantern stock
+
+### Audio
+
+- 4no. patchable passive speakers in surround configuration
+- Allen and Heath QU16
+- 8no. XLR ties at low-level facility panels
+- Access to TechSoc's microphone stock
+
+### Communication Systems
+
+- Single Channel comms system provision
+- Comms system available through TechSoc
