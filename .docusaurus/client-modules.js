a3ee2a524855ae0465dc6290c70b9739d44c6f0a
John Cherry <github@johncherry.me>
2023-05-20 17:18:01 +0100

    Remove node_modules

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/workspaces/wiki/node_modules/infima/dist/css/default/default.css'),
-  require('/workspaces/wiki/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/workspaces/wiki/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/workspaces/wiki/src/css/custom.css'),
-];
